51e2b0856268e5593fe6f2606ad7ffc5ac7dd7a5
Felipe Rocha <feliperochaoliveira@gmail.com>
2022-10-23 13:16:51 -0300

    Add Modal config and global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import Modal from 'react-modal';
 import Routes from './routes';
 
 import GlobalStyle from './styles/global';
 
-const App: React.FC = () => (
-  <>
-    <GlobalStyle />
-    <Router>
-      <Routes />
-    </Router>
-  </>
-);
+Modal.setAppElement('#root');
+
+const App: React.FC = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <Router>
+        <Routes />
+      </Router>
+    </>
+  );
+};
 
 export default App;
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
+  :root {
+    --background: #F0F2F5;
+  }
   * {
     margin: 0;
     padding: 0;
@@ -9,7 +12,7 @@ export default createGlobalStyle`
   }
 
   body {
-    background: #F0F2F5 ;
+    background: var(--background);
     -webkit-font-smoothing: antialiased
   }
 
@@ -19,5 +22,42 @@ export default createGlobalStyle`
 
   button {
     cursor: pointer;
+  }
+
+  .react-modal-overlay {
+    background: rgba(0, 0, 0, 0.5);
+
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .react-modal-content {
+    width: 100%;
+    max-width: 576px;
+    background: var(--background);
+    padding: 3rem;
+    position: relative;
+    border-radius: 0.24rem;
+  }
+
+  .react-modal-close {
+    position: absolute;
+    right: 1.5rem;
+    top: 1.5rem;
+    border: 0;
+    background: transparent;
+
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.8)
+    }
   }
 `;
