f66df93328a5310008c8cf5668fc24ff5141c0bc
Felipe Rocha <feliperochaoliveira@gmail.com>
2022-10-23 13:21:50 -0300

    Add context on route component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
-
 import { Switch, Route } from 'react-router-dom';
+import NewTransactionModal from '../components/NewTransactionModal';
+import { TransactionProvider } from '../contexts/TransactionContext';
 
 import Dashboard from '../pages/Dashboard';
 import Import from '../pages/Import';
 
-const Routes: React.FC = () => (
-  <Switch>
-    <Route path="/" exact component={Dashboard} />
-    <Route path="/import" component={Import} />
-  </Switch>
-);
+const Routes: React.FC = () => {
+  return (
+    <TransactionProvider>
+      <Switch>
+        <Route path="/" exact component={Dashboard} />
+        <Route path="/import" component={Import} />
+      </Switch>
+      <NewTransactionModal />
+    </TransactionProvider>
+  );
+};
 
 export default Routes;
